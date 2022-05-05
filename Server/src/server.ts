f875d84c803503412c7f322bb62e3d0b77ea5669
carlosGodoi <carlos-godoi@hotmail.com>
2022-05-05 17:59:48 -0300

    Iniciando a criação do banco de dados

---
+++ Server/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET = Buscar informações
+// POST = Cadastrar informações
+// PUT = Atualizar informações de uma entidade
+// PATCH = Atualizar uma informação única de uma entidade
+// DELETE = Deletar uma informação
+
+app.post('/feedbacks', (req, res) => {
+  return res.send('Hello World');
+})
+
+app.listen(3333, () =>{
+  console.log('HTTP server running!')
+})
+
